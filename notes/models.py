707ebb0369de4d1f3d39472183d1cf9dbe622927
disanem <erik1skornyakov@gmail.com>
2016-03-02 20:39:53 +0300

    add Note model

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,9 @@
+import datetime
+
 from django.db import models
 
 # Create your models here.
+class Note(models.Model):
+    """ Database table"""
+    note_text = models.TextField()
+    pub_date = models.DateTimeField(default=datetime.now())
