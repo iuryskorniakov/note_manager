742df70f1da0f474cc0d68e99636c65bbdb2d14c
disanem <erik1skornyakov@gmail.com>
2016-03-03 08:08:58 +0300

    some changes

--- notes/urls.py
+++ notes/urls.py
@@ -1,7 +1,7 @@
-from .views import login
+from .views import login_view, logout_view
 from django.conf.urls import url
 
 urlpatterns = [
-    url(r'^login', login),
-    url(r'logout')
+    url(r'^login', login_view),
+    url(r'logout', logout_view),
 ]
