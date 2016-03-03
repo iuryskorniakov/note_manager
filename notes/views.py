4a17530b08c2f96530dee5cdc516e021b6a2851d
disanem <erik1skornyakov@gmail.com>
2016-03-03 06:14:51 +0300

    add Note view

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Note
+import uuid
 
 # Create your views here.
+def home(request):
+    """
+    Main page
+    """
+    context = Note.objects.order_by('-pub_date')
+    return render(request, 'notes/home.html', )
